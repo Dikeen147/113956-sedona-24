4548bf173f755e42adc76f71cd9022a8c782736e
Константин <45126282+Dikeen147@users.noreply.github.com>
2019-02-27 23:24:14 +0300

    error anim js

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,38 @@
 // ПОКАЗАТЬ ФОРМУ ПОИСКА ГОСТИНИЦЫ
 var searchHostelBtn = document.querySelector(".search-hostel-btn");
 var popup = document.querySelector(".search-hostel");
-
+var form = document.querySelector(".search-hostel");
 var arrival = popup.querySelector("[name=arrival]");
+var departure = popup.querySelector("[name=departure]");
+
+var isStorageSupport = true;
+var storage = "";
+
 var bool = true;
 	
+try {
+	storage = localStorage.getItem("arrival");
+} catch (err) {
+	isStorageSupport = false;
+}
+
+form.addEventListener("submit", function(evt){
+	if (!arrival.value || !departure.value) {
+		if (popup.classList.contains("clear-field")) {
+			evt.preventDefault();
+			popup.classList.remove("clear-field");
+		} 
+			evt.preventDefault();
+			popup.classList.add("clear-field");
+			console.log("нужно ввести логин и пароль");	
+	} else {
+		if (isStorageSupport) {
+			localStorage.setItem("arrival", arrival.value);
+		}
+	}
+});
+
 searchHostelBtn.addEventListener("click", function(evt){
-	/*popup.classList.remove("visually-show2");*/
-	
 	if (popup.classList.contains("visually-close")) {
 		evt.preventDefault();
 		popup.classList.remove("visually-close");
@@ -22,6 +47,12 @@ searchHostelBtn.addEventListener("click", function(evt){
 		evt.preventDefault();
 		popup.classList.add("visually-show");	
 		arrival.focus();
+		if (storage) {
+			arrival.value = storage;
+			departure.focus();
+		} else {
+			arrival.focus();
+		}
 	}
 	
 });
@@ -35,7 +66,7 @@ window.addEventListener("keydown", function(evt){
 	}
 });
 
-
+/****************************************************************************/
 // В форме поиска гостиницы при нажатии минуса или плюса, убваляет либо прибавляет
 var adultsInput = document.querySelector(".adults-input");
 var minus1 = adultsInput.querySelector(".minus");
